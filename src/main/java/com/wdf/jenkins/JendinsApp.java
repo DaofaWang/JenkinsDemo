f7da2d7b6d5107c1fed3fd35ac8ddff417b4a74a
DougWang <3504614357@qq.com>
2023-03-12 10:23:39 +0800

    test webapp run

---
+++ src/main/java/com/wdf/jenkins/JendinsApp.java
@@ -0,0 +1,18 @@
+package main.java.com.wdf.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Date: 2023-03-11
+ * @Author: wdf
+ * @Description:
+ */
+@SpringBootApplication
+public class JendinsApp {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JendinsApp.class,args);
+    }
+
+}
---
+++ src/main/java/com/wdf/jenkins/controller/IndexController.java
@@ -0,0 +1,17 @@
+package main.java.com.wdf.jenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Date: 2023-03-11
+ * @Author: wdf
+ * @Description:
+ */
+@RestController
+public class IndexController {
+    @RequestMapping({"/"})
+    public String hello(){
+        return "hello";
+    }
+}
